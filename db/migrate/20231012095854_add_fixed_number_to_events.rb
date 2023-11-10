02d4d919b59b7817103a8e7898b3f8c5a2eabd2a
EC2 Default User <ec2-user@ip-172-31-36-88.ap-northeast-1.compute.internal>
2023-11-10 12:00:27 +0000

    [Fix] migration files

--- db/migrate/20231012095854_add_fixed_number_to_events.rb
+++ db/migrate/20231012095854_add_fixed_number_to_events.rb
@@ -1,5 +1,5 @@
 class AddFixedNumberToEvents < ActiveRecord::Migration[6.1]
   def change
-    add_column :events, :fixed_number, :intger
+    add_column :events, :fixed_number, :integer
   end
 end
