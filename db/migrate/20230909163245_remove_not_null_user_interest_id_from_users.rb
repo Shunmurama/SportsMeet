f89edd7c75374b5fbb66ce0fc1443b3a55982af7
EC2 Default User <ec2-user@ip-172-31-36-88.ap-northeast-1.compute.internal>
2023-11-10 11:59:07 +0000

    [Fix] migration files

--- db/migrate/20230909163245_remove_not_null_user_interest_id_from_users.rb
+++ db/migrate/20230909163245_remove_not_null_user_interest_id_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveNotNullUserInterestIdFromUsers < ActiveRecord::Migration[6.1]
   def change
-    change_column :users, :user_interest_id, :integer, null: true
+    # change_column :users, :user_interest_id, :integer, null: true
   end
 end
